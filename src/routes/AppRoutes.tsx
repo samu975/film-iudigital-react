e8021d32ee6ba9b5a4aff2e81fc2892262ac1f5f
Samuel Rosero <saferoxx215@gmail.com>
2024-09-19 20:28:06 -0500

    move routes to app.tsx

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import DirectorDetail from '../pages/Director/DirectorDetail';
 import DirectorForm from '../pages/Director/DirectorForm';
 import DirectorList from '../pages/Director/DirectorList';
@@ -11,37 +11,37 @@ import GenreList from '../pages/Genre/GenreList';
 import ProducerDetail from '../pages/Producer/ProducerDetail';
 import ProducerForm from '../pages/Producer/ProducerForm';
 import ProducerList from '../pages/Producer/ProducerList';
+import Home from '../pages/Home/Home';
 
 const AppRoutes = () => {
   return (
-    <Router>
-      <Routes>
-        {/* Director */}
-        <Route path="/director/:id" element={<DirectorDetail />} />
-        <Route path="/director/form" element={<DirectorForm />} />
-        <Route path="/director/list" element={<DirectorList />} />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      {/* Director */}
+      <Route path="/director/:id" element={<DirectorDetail />} />
+      <Route path="/director/form" element={<DirectorForm />} />
+      <Route path="/director/list" element={<DirectorList />} />
 
-        {/* Film */}
-        <Route path="/film/:id" element={<FilmDetail />} />
-        <Route path="/film/form" element={<FilmForm />} />
-        <Route path="/film/list" element={<FilmList />} />
+      {/* Film */}
+      <Route path="/film/:id" element={<FilmDetail />} />
+      <Route path="/film/form" element={<FilmForm />} />
+      <Route path="/film/list" element={<FilmList />} />
 
-        {/* Genre */}
-        <Route path="/genre/:id" element={<GenreDetail />} />
-        <Route path="/genre/form" element={<GenreForm />} />
-        <Route path="/genre/list" element={<GenreList />} />
+      {/* Genre */}
+      <Route path="/genre/:id" element={<GenreDetail />} />
+      <Route path="/genre/form" element={<GenreForm />} />
+      <Route path="/genre/list" element={<GenreList />} />
 
-        {/* Producer */}
-        <Route path="/producer/:id" element={<ProducerDetail />} />
-        <Route path="/producer/form" element={<ProducerForm />} />
-        <Route path="/producer/list" element={<ProducerList />} />
+      {/* Producer */}
+      <Route path="/producer/:id" element={<ProducerDetail />} />
+      <Route path="/producer/form" element={<ProducerForm />} />
+      <Route path="/producer/list" element={<ProducerList />} />
 
-        {/* Types
+      {/* Types
         Agregar si se hacen tipes
         <Route path="/types/series" element={<Series />} />
         <Route path="/types/movies" element={<Movies />} /> */}
-      </Routes>
-    </Router>
+    </Routes>
   );
 };
 
