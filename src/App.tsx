0718a77be9eb33f2fc30e856659659fa8c5ff5d0
Samuel Rosero <saferoxx215@gmail.com>
2024-09-24 22:03:43 -0500

    Merge aleicer branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Layout from './layout/layout';
 import AppRoutes from './routes/AppRoutes';
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css'
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
